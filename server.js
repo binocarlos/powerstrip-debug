e2957ade84bbfdc089daa56747d21b5f2b952f10
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-22 23:24:24 +0000

    move core handler into server module

---
+++ server.js
@@ -0,0 +1,33 @@
+var concat = require('concat-stream')
+var Logger = require('./logger')
+var Response = require('./response')
+
+module.exports = function(opts){
+  opts = opts || {}
+  return function(req, res){
+    req.pipe(concat(function(body){
+      if(!body){
+        res.statusCode = 500;
+        res.end('No POST body found')
+        return
+      }
+
+      body = JSON.parse(body.toString())
+
+      var response = Response(body)
+      var output = Logger(body)
+
+      if(!response){
+        console.log('Error:  No Type found in body')
+        res.statusCode = 500;
+        res.end('No Type found in body')
+        return
+      }
+      
+      console.log(output)
+      
+      res.setHeader('Content-type', 'application/json')
+      res.end(JSON.stringify(response, null, 4))
+    }))
+  }
+}
