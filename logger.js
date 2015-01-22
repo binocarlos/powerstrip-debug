069eaa1dd247422a3fc04aa6063939151ebf5c70
Kai Davenport <kaiyadavenport@gmail.com>
2015-01-22 23:03:52 +0000

    extract logger into module

---
+++ logger.js
@@ -0,0 +1,68 @@
+module.exports = function(body){
+  var parts = [
+    '',
+    '-------------------------------------------',
+    '',
+    'Version:          ' + body.PowerstripProtocolVersion,
+    'Type:             ' + body.Type
+  ]
+
+  if(body.Type=='pre-hook'){
+
+    var ClientRequest = body.ClientRequest
+    var ClientRequestBody = JSON.parse(ClientRequest.Body)
+
+    parts = parts.concat([
+      'Request:',
+      '',
+      '    Method:      ' + ClientRequest.Method,
+      '    URL:         ' + ClientRequest.Request
+    ])
+
+    if(ClientRequestBody){
+      parts = parts.concat([
+        '',
+        JSON.stringify(ClientRequestBody, null, 4)
+      ])
+    }
+
+    parts.push('')
+  }
+  else if(body.Type=='post-hook'){
+
+    var ClientRequest = body.ClientRequest
+    var ClientRequestBody = JSON.parse(ClientRequest.Body)
+
+    var ServerResponse = body.ServerResponse
+
+    parts = parts.concat([
+      'Request:',
+      '',
+      '    Method:      ' + ClientRequest.Method,
+      '    URL:         ' + ClientRequest.Request
+    ])
+
+    if(ClientRequestBody){
+      parts = parts.concat([
+        '',
+        JSON.stringify(ClientRequestBody, null, 4)
+      ])
+    }
+
+    parts = parts.concat([
+      '',
+      'Response:',
+      '',
+      '    ContentType: ' + ServerResponse.ContentType,
+      '    Code:        ' + ServerResponse.Code,
+      '',
+      ServerResponse.Body
+    ])
+    
+  }
+  else{
+    parts.push('Error:  No Type found in body')
+  }
+
+  return parts.join("\n")
+}
